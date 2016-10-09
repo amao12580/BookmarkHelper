33052020c584c17bf2e3bcc2f055cfef301a3284
Steven Cheng <chengliangchengliang888@gmail.com>
2016-10-09 18:45:34 +0800

    补全string.xml 最佳实践

--- app/src/main/java/pro/kisscat/www/bookmarkhelper/converter/support/Broswer.java
+++ app/src/main/java/pro/kisscat/www/bookmarkhelper/converter/support/Broswer.java
@@ -17,6 +17,15 @@ import lombok.Setter;
  * Time:15:31
  */
 
+/**
+ *      @Data   ：注解在类上；提供类所有属性的 getting 和 setting 方法，此外还提供了equals、canEqual、hashCode、toString 方法
+ *      @Setter：注解在属性上；为属性提供 setting 方法
+ *      @Getter：注解在属性上；为属性提供 getting 方法
+ *      @Log4j ：注解在类上；为类提供一个 属性名为log 的 log4j 日志对象
+ *      @NoArgsConstructor：注解在类上；为类提供一个无参的构造方法
+ *      @AllArgsConstructor：注解在类上；为类提供一个全参的构造方法
+ */
+
 public abstract class Broswer {
     @Getter
     private Context context;
